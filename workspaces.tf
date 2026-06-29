8cf4f42862cf30769bf0b717529cbddb9bd49395
Jakub Sokołowski <jakub@status.im>
2026-06-29 15:56:56 +0200

    lez: move from dev to test fleet
    https://github.com/status-im/infra-logos/issues/29
    
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- workspaces.tf
+++ workspaces.tf
@@ -115,9 +115,7 @@ locals {
 
       do_chat_kc_count = 1
 
-      lez_node_ips = [
-        "65.108.237.32", # lez-01.he-eu-hel1.logos.dev
-      ]
+      lez_node_ips = []
     }
     test = {
       ac_node_count = 2
@@ -139,6 +137,10 @@ locals {
       node_db_gc_type = "c2d-highcpu-2"
 
       node_db_data_vol_size = 320
+
+      lez_node_ips = [
+        "65.108.237.32", # lez-01.he-eu-hel1.logos.test
+      ]
     }
   }
 }
